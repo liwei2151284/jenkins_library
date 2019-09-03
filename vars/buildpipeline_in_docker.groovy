840a452d316b3ea086151ca3126fef94f6580e1d
liwei2151284 <445169331@qq.com>
2019-09-03 17:46:03 +0800

    Update buildpipeline_in_docker.groovy

--- vars/buildpipeline_in_docker.groovy
+++ vars/buildpipeline_in_docker.groovy
@@ -8,7 +8,7 @@ def call(String giturl,jdk_version) {
     java_home = '/usr/lib/jvm/java-8-openjdk-amd64'
   }
   podTemplate(label: 'jenkins-slave', cloud: 'kubernetes',containers: [
-      containerTemplate(name: 'jenkins-slave', envVars: [envVar(key: 'JAVA_HOME', value: '/usr/lib/jvm/java-8-openjdk-amd64')],image: 'akagelo/jenkins-slave-maven3:latest', ttyEnabled: true, command: 'cat'),
+      containerTemplate(name: 'jenkins-slave', envVars: [envVar(key: 'JAVA_HOME', value: "${java_home}")],image: "${image}", ttyEnabled: true, command: 'cat'),
     ]) {
       node('jenkins-slave') {
           stage('git-checkout') {
@@ -27,7 +27,7 @@ def call(String giturl,jdk_version) {
 
           stage('artifactory config') {
               container('jenkins-slave') {
-                  env.JAVA_HOME = '/usr/lib/jvm/java-8-openjdk-amd64'
+                  env.JAVA_HOME = "${java_home}"
                   rtMaven.tool = 'maven-k8s' // Tool name from Jenkins configuration
                   rtMaven.deployer releaseRepo: 'jenkins_pipeline_webinar_stage_local', snapshotRepo: 'jenkins_pipeline_webinar_snapshot_local', server: artiServer
                   rtMaven.resolver releaseRepo: 'jenkins_pipeline_webinar_release_virtual', snapshotRepo: 'jenkins_pipeline_webinar_release_virtual', server: artiServer
@@ -36,7 +36,7 @@ def call(String giturl,jdk_version) {
         
           stage ('Exec Maven') {
               container('jenkins-slave') {
-                  env.JAVA_HOME = '/usr/lib/jvm/java-8-openjdk-amd64'
+                  env.JAVA_HOME = "${java_home}"
                   rtMaven.run pom: 'maven-example/multi3/pom.xml', goals: 'clean install -U', buildInfo: buildInfo
               }
           }
